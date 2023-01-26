7ed8a3e36bb431b9d6d98b86417f480adbb62d29
Gavin Rees <gavinrees@college.harvard.edu>
2023-01-26 15:09:55 -0500

    Rewrote to use a simple mixed strategy model for each sample, not a linear classifier, in order to elucidate the game dynamics. I think the issue here is that there is no real connection between the games (one per sample) and there's no reason that the agents should really condition on them. You could maybe remove the biases from a linear classifier but that still seems like something that a deep net would get around. So next step, going to incorporate some ideas from "Unsupervised Classifiers, Mutual Information and ' Phantom Targets'" to try and get them to play the game while also maximizing the mutual information of their outputs and the training data.

---
+++ SimpleStratExperiment.m
@@ -0,0 +1,63 @@
+%% Set up
+data = (1:2)'; %each sample is a row (like database row)
+
+antiCoordGame = [0 7; 5 0];
+coordGame = [7 0; 0 5]; %note the imbalance!
+epochs = 5000;
+lr = 0.001;
+N = 50;
+sim_payoff_tracking = zeros([N,3]);
+sim_sensitivity_tracking = zeros([N 3]);
+
+
+%% Train
+tic;
+for simInd=1:N
+    alice = rand([1 2]);
+    bob = rand([1 2]);
+    eve = rand([1 2]);
+    payoff_tracking = zeros([epochs 3]);
+    sensitivity_tracking = zeros([epochs 3]);
+    numSamples = size(data, 1);
+    for epoch=1:epochs
+        aliceGrads = zeros([1 2]);
+        bobGrads = zeros([1 2]);
+        eveGrads = zeros([1 2]);
+        for sampleInd=1:size(data,1)
+            s = data(sampleInd,:); %a ROW of data
+            
+            ys = [alice(s) (1- alice(s));
+                  bob(s) (1- bob(s));
+                  eve(s) (1-eve(s))];
+
+             payoffs = [ys(1,:) * antiCoordGame * (ys(2,:)' + ys(3,:)'); %alice
+                        ys(2,:) * antiCoordGame * (ys(1,:)' + ys(3,:)'); %bob
+                        ys(3,:) * coordGame * (ys(1,:)' + ys(2,:)')]; %eve
+
+            payoff_tracking(epoch,:) = payoff_tracking(epoch,:) + payoffs'/numSamples;
+            
+            % alice and bob ascend anti-coordination game payoff
+            aliceGrads(s) = aliceGrads(s) + [1 -1] * antiCoordGame * (ys(2,:) + ys(3,:))';
+            bobGrads(s) = bobGrads(s) + [1 -1] * antiCoordGame * (ys(1,:) + ys(3,:))';
+            % eve gradient ascends coordination game payoff
+            eveGrads(s) = eveGrads(s) + [1 -1] * coordGame * (ys(1,:) + ys(2,:))';
+        end
+        % Gradient ascend payoff
+        alice = alice + lr * aliceGrads ./ numSamples;
+        bob = bob + lr * bobGrads ./ numSamples;
+        eve = eve + lr * eveGrads ./ numSamples;
+        % Clamp to [0,1] probability range
+        alice = min(max(alice, zeros('like', alice)), ones('like', alice));
+        bob = min(max(bob, zeros('like', bob)), ones('like', bob));
+        eve = min(max(eve, zeros('like', eve)), ones('like', eve));
+
+        %Tracking statistics
+        aliceSensitivity = norm(alice(1) - alice(2));
+        bobSensitivity = norm(bob(1) - bob(2));
+        eveSensitivity = norm(eve(1) - eve(2));
+        sensitivity_tracking(epoch,:) = [aliceSensitivity bobSensitivity eveSensitivity];
+    end
+    sim_payoff_tracking(simInd,:) = mean(payoff_tracking((epochs * 0.9):epochs,:),1);
+    sim_sensitivity_tracking(simInd, :) = mean(sensitivity_tracking((epochs * 0.9):epochs,:),1);
+end
+toc
