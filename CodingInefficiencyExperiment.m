e634c52b90893573853436ac33885c86ce593e1c
Gavin Rees <gavinrees@college.harvard.edu>
2023-02-08 15:34:07 -0500

    Initial attempt at explicit gradients -- not working as expected. It's a pretty messy calculation and not sure whether the error is in the theory or the implementation

--- CodingInefficiencyExperiment.m
+++ CodingInefficiencyExperiment.m
@@ -9,46 +9,93 @@
 % given a "heads" from bob/alice's coin, and the output of bob/alice, what
 % is the average coding inefficiency of alice/bob's weighted coin?
 
-numSamples = 4;
-data = (1:numSamples)'; %each sample is a row (like a database row)
+M = 2;
+data = (1:M)'; %each sample is a row (like a database row)
 epochs=10000;
-lr = 0.001;
+lr = 0.1;
 
 tic;
-alice = rand(1,numSamples);
-bob = rand(1,numSamples);
+% alice = rand(1,M);
+alice = [0.5, 0.5];
+bob = [0.5, 0.5];
 payoff_tracking = zeros(epochs,2);
 
 for epoch=1:epochs
-    %% Establish baseline payoffs
-    payoffs = inefficiencyPayoffs(data, alice,bob);
+    %% Track payoffs
+    payoffs = inefficiencyPayoffs(data, alice, bob);
     payoff_tracking(epoch,:) = payoffs;
 
-    %% Approximate gradients for Alice
-    aSteps = zeros(1,numSamples);
-    for perturbInd=1:numSamples
-        perturbation = zeros(1,numSamples);
-        perturbation(perturbInd) = lr;
-        perturbedPayoffs = inefficiencyPayoffs(data, alice + perturbation, bob);
-        aSteps(perturbInd) = lr * (perturbedPayoffs(1) > payoffs(1)) ...
-                            - lr * (perturbedPayoffs(1) < payoffs(1));
-    end
+    %% Calculate statistics
+    jDist = jointDistribution(data, alice, bob);
+    aDist = sum(jDist, 2);
+    aBar = aDist(1);
+    bDist = sum(jDist, 1);
+    bBar = bDist(1);
 
-    %% Approximatee gradients for Bob
-    bSteps = zeros(1, numSamples);
-    for perturbInd=1:numSamples
-        perturbation = zeros(1,numSamples);
-        perturbation(perturbInd) = lr;
-        perturbedPayoffs = inefficiencyPayoffs(data, alice, bob + perturbation);
-        bSteps(perturbInd) = lr * (perturbedPayoffs(2) > payoffs(2)) ...
-                            - lr * (perturbedPayoffs(2) < payoffs(2));
-    end
+    distACondB = jDist(:,1) / sum(jDist(:,1));
+    pACondB = distACondB(1);
+    distACondBc = jDist(:,2) / sum(jDist(:, 2));
+    pACondBc = distACondBc(1);
 
-    %% Gradient ascend payoffs
-    alice = alice + aSteps;
-    bob = bob + bSteps;
+    distBCondA = jDist(1,:) / sum(jDist(1,:));
+    pBCondA = distBCondA(1);
+    distBCondAc = jDist(2,:) / sum(jDist(2,:));
+    pBCondAc = distBCondAc(1);
 
-    alice = min(max(alice, 0.001), 0.999);
+    %% Calculate samplewise conditionals
+    pACondb_k = pACondB * bob' + pACondBc * (1-bob');
+    pBConda_k = pBCondA * alice' + pBCondAc * (1-alice');
+
+    
+    %% Calculate Alice gradients
+    %aGrad(k,i) = d pi_a_k / d a_i
+    
+    % Derivative of H(abar).
+    dHofAbar = - ones(M,M) / M * (log(aBar) - log(1-aBar));
+    
+    % Derivative of L(a_k, abar)
+    dLa_k__abar = -ones(M,M)/M .* (alice'./aBar - (1-alice')./(1-aBar));
+    dLa_k__abar = dLa_k__abar - diag(log(aBar) - log(1-aBar) .* ones(1,M));
+
+    % Derivative of H(P(A|b_k))
+    dHPACondb_k = -ones(M,M)/M .* (log(pACondb_k) - log(1-pACondb_k)) .* ...
+                    (bob' * bob ./ bBar + (1-bob') * (1-bob) ./ (1-bBar));
+
+    % Derivative of L(a_k, P(A|b_k))
+    dLa_k__PACondb_k = -ones(M,M)/M * (log(pACondb_k) - log(1-pACondb_k)) .* ...
+                        (bob'  .* bob / bBar + (1-bob') .* (1-bob) / bBar);
+    dLa_k__PACondb_k = dLa_k__PACondb_k - diag(log(pACondb_k) - log(1-pACondb_k));
+
+    aGrads = dLa_k__abar - dHofAbar + dLa_k__PACondb_k - dHPACondb_k;
+    ASampleGrads = ones(1,M) / M * aGrads;
+
+    %% Calculate Bob gradients
+    %bGrad(k,i) = d pi_b_k / d b_i
+    
+    % Derivative of H(bbar).
+    dHofBbar = - ones(M,M) / M * (log(bBar) - log(1-bBar));
+    
+    % Derivative of L(b_k, bbar)
+    dLb_k__bbar = -ones(M,M)/M .* (bob'./bBar - (1-bob')./(1-bBar));
+    dLb_k__bbar = dLb_k__bbar - diag(log(bBar) - log(1-bBar) .* ones(1,M));
+
+    % Derivative of H(P(B|a_k))
+    dHPBConda_k = -ones(M,M)/M .* (log(pBConda_k) - log(1-pBConda_k)) .* ...
+                    (alice' * alice ./ aBar + (1-alice') * (1-alice) ./ (1-aBar));
+
+    % Derivative of L(b_k, P(B|a_k))
+    dLb_k__PBConda_k = -ones(M,M)/M * (log(pBConda_k) - log(1-pBConda_k)) .* ...
+                        (alice'  .* alice / aBar + (1-alice') .* (1-alice) / aBar);
+    dLb_k__PBConda_k = dLb_k__PBConda_k - diag(log(pBConda_k) - log(1-pBConda_k));
+
+    bGrads = dLb_k__bbar - dHofBbar + dLb_k__PBConda_k - dHPBConda_k;
+    BSampleGrads = ones(1,M) / M * bGrads;
+
+    %% Gradient ascend
+
+%     alice = alice + lr * ASampleGrads;
+    bob = bob + lr * BSampleGrads;
+%     alice = min(max(alice, 0.001), 0.999);
     bob = min(max(bob, 0.001), 0.999);
 
 end
